5d0b66175ce209be7a9fff4bdd79ceb4a5f3a355
chowdaiahgorige <gorigechowdaiah371@gmail.com>
2025-12-14 16:57:55 +0530

    new changes

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const btn = document.querySelector(".btn9");
 const form = document.querySelector("form");
 const para = document.querySelector("#para001");
-const book=document.querySelector(".section4")
+const book = document.querySelector(".section4");
 
+const btn2 = document.querySelectorAll(".btn2"); // Add buttons
+const btn11 = document.querySelectorAll(".btn1"); // Remove buttons
+
+// BOOK SERVICE BUTTON
 btn.addEventListener("click", (event) => {
     event.preventDefault();
 
@@ -19,10 +23,25 @@ btn.addEventListener("click", (event) => {
         return;
     }
 
-    // Show final message
-     
-     para.innerHTML = " “Thank you ForBooking the Service We will get back to you soon!”.";
-
-    
+    para.innerHTML =
+        "Thank you for booking the service. We will get back to you soon!";
 });
+
+// ADD ITEM BUTTONS
+btn2.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        alert("Please add items");
+    });
+});
+
+// REMOVE ITEM BUTTONS
+btn11.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (confirm("Are you sure to remove?")) {
+            btn.parentElement.remove();
+        }
+    });
+});
+
+
 
